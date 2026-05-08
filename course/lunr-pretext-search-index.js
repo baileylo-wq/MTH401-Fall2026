78cb7109f242fe1960d8a96105dafd33cd0e4e80
Lora Bailey <baileylo@gvsu.edu>
2026-05-08 16:21:26 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,131 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "syllabus",
+  "level": "1",
+  "url": "syllabus.html",
+  "type": "Section",
+  "number": "",
+  "title": "Syllabus",
+  "body": " Syllabus        Course Information  This is the syllabus for course name (MATH xxx, section xxx) for [term] 20xx. It is a [n] credit course.    Instructor  Prof. Lastname, Office Location, prof.lastname@example.edu .    Student Hours  TBD    Class meets  course times and location.    Course Description  course description from catalog    Prerequisite  list of prerequisites    Textbook and course materials   textbook name by textbook author.       Course Overview        Assessments and Grades     "
+},
+{
+  "id": "sec-course-info-2",
+  "level": "2",
+  "url": "syllabus.html#sec-course-info-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "course name (MATH xxx, section xxx) "
+},
+{
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/22      Wednesday 8\/24      Friday 8\/26     "
+},
+{
+  "id": "notes-week-02",
+  "level": "1",
+  "url": "notes-week-02.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 2",
+  "body": " Week 2   Monday      Wednesday      Friday     "
+},
+{
+  "id": "activity-01-intro-activity",
+  "level": "1",
+  "url": "activity-01-intro-activity.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Introduction Activity",
+  "body": " Introduction Activity    This is the introduction to the activity.      This is the first exercise.    "
+},
+{
+  "id": "activity-01-intro-activity-3",
+  "level": "2",
+  "url": "activity-01-intro-activity.html#activity-01-intro-activity-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  This is the first exercise.   "
+},
+{
+  "id": "activities-4",
+  "level": "1",
+  "url": "activities-4.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "1. Review of Calculus III",
+  "body": " 1. Review of Calculus III  MTH 401    Consider the vectors      Find the dot products and .                How does this help you find the length of ?       What is the angle between and ?       How can the dot product detect when two vectors are orthogonal?       If is a vector and , what does this say about ?       Find the cross product .       Describe in words how you would find the direction in which points.       What is ?         Shown below are a line and a circle.     insert alt text here    f(x)=1.5-1\/2*x          insert alt text here               Create a parametrization for each of the curves.      What choices have you made in forming your parametrizations and how might your parametrizations differ from a classmate's?       Consider the function .     Find the gradient .      If you are at the point , what is the gradient there?      Suppose is measuring the elevation of a landscape in feet. In what direction should you move from the point to move most steeply uphill? In what direction should you move to stay at the same elevation?      Suppose your motion in the plane is given by the function . What is your location at time and what is your velocity?      If is measured in seconds, at what rate, in feet per second, are you moving uphill at ?       Sketch the field at the nine points where and take on the values , , and . (You don't necessarily need to draw the vectors to scale.)    Prefigure - line integrals for worksheet 2   insert alt text here             "
+},
+{
+  "id": "exr-x",
+  "level": "2",
+  "url": "activities-4.html#exr-x",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Consider the vectors      Find the dot products and .                How does this help you find the length of ?       What is the angle between and ?       How can the dot product detect when two vectors are orthogonal?       If is a vector and , what does this say about ?       Find the cross product .       Describe in words how you would find the direction in which points.       What is ?     "
+},
+{
+  "id": "activities-4-4",
+  "level": "2",
+  "url": "activities-4.html#activities-4-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Shown below are a line and a circle.     insert alt text here    f(x)=1.5-1\/2*x          insert alt text here               Create a parametrization for each of the curves.      What choices have you made in forming your parametrizations and how might your parametrizations differ from a classmate's?    "
+},
+{
+  "id": "activities-4-5",
+  "level": "2",
+  "url": "activities-4.html#activities-4-5",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Consider the function .     Find the gradient .      If you are at the point , what is the gradient there?      Suppose is measuring the elevation of a landscape in feet. In what direction should you move from the point to move most steeply uphill? In what direction should you move to stay at the same elevation?      Suppose your motion in the plane is given by the function . What is your location at time and what is your velocity?      If is measured in seconds, at what rate, in feet per second, are you moving uphill at ?    "
+},
+{
+  "id": "activities-4-6",
+  "level": "2",
+  "url": "activities-4.html#activities-4-6",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Sketch the field at the nine points where and take on the values , , and . (You don't necessarily need to draw the vectors to scale.)   "
+},
+{
+  "id": "handouts",
+  "level": "1",
+  "url": "handouts.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Handouts",
+  "body": " Handouts    "
+},
+{
+  "id": "homework",
+  "level": "1",
+  "url": "homework.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Homework",
+  "body": " Homework    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
